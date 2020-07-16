f5cddb5bf16287016560dd871c110ee4159c9121
Maria Vatolina <mary4erry@yandex.ru>
2020-07-16 16:57:02 +0300

    Updates

---
+++ precache-manifest.355d4b980e4cc2f075a94598c6547ca1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae75017ddc89bf8de5d3bbaae39c199",
+    "url": "/social-net/index.html"
+  },
+  {
+    "revision": "55c54db4191c48e272f0",
+    "url": "/social-net/static/css/3.030b1ab7.chunk.css"
+  },
+  {
+    "revision": "4ddcb7389d13a05b6ec2",
+    "url": "/social-net/static/css/4.f3a38624.chunk.css"
+  },
+  {
+    "revision": "f7aa5fde42d2e7652618",
+    "url": "/social-net/static/css/main.cc4cb274.chunk.css"
+  },
+  {
+    "revision": "00c51db2f7fc638295b5",
+    "url": "/social-net/static/js/2.6d0f84ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-net/static/js/2.6d0f84ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c54db4191c48e272f0",
+    "url": "/social-net/static/js/3.12be033f.chunk.js"
+  },
+  {
+    "revision": "4ddcb7389d13a05b6ec2",
+    "url": "/social-net/static/js/4.2fbd1e47.chunk.js"
+  },
+  {
+    "revision": "f7aa5fde42d2e7652618",
+    "url": "/social-net/static/js/main.d3234cdb.chunk.js"
+  },
+  {
+    "revision": "6c25f4132cc9477b1e7f",
+    "url": "/social-net/static/js/runtime-main.3f0daf14.js"
+  },
+  {
+    "revision": "720bf76fe9c95e57257ad8e00421f467",
+    "url": "/social-net/static/media/preloader.720bf76f.svg"
+  }
+]);
